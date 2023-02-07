abb7861a5649323b76cdf333e039cfa192720379
Valer BOCAN <vbocan@gmail.com>
2023-02-07 07:41:11 +0200

    Records in own file

--- IPFEngine/IPFEngine.Parser/Parser.cs
+++ IPFEngine/IPFEngine.Parser/Parser.cs
@@ -306,33 +306,4 @@ namespace IPFEngine.Parser
     {
         None, List, Number, Boolean, Fee, FeeCase
     }
-
-    public abstract record IPFVariable(string Name, string Text);
-    public record IPFVariableBoolean(string Name, string Text, bool DefaultValue) : IPFVariable(Name, Text);
-    public record IPFVariableList(string Name, string Text, IList<IPFListItem> Items, string DefaultSymbol) : IPFVariable(Name, Text);
-    public record IPFListItem(string Symbol, string Value);
-    public record IPFVariableNumber(string Name, string Text, int MinValue, int MaxValue, int DefaultValue) : IPFVariable(Name, Text);
-
-    public abstract record IPFItem(IEnumerable<string> Condition);
-    public record IPFFee(string Name, IList<IPFItem> Cases)
-    {
-        public override string ToString()
-        {
-            return string.Format("\n\rFEE: {0}\n\r{1}", Name, string.Join(Environment.NewLine, Cases));
-        }
-    }
-    public record IPFFeeCase(IEnumerable<string> Condition, IList<IPFFeeYield> Yields) : IPFItem(Condition)
-    {       
-        public override string ToString()
-        {
-            return string.Format("CASE: {0}\n\r{1}", string.Join(" ", Condition), string.Join(Environment.NewLine, Yields));
-        }
-    }
-    public record IPFFeeYield(IEnumerable<string> Condition, IEnumerable<string> Values) : IPFItem(Condition)
-    {
-        public override string ToString()
-        {
-            return string.Format("YIELD: {0} CONDITION: {1}", string.Join(" ", Values), string.Join(" ", Condition));
-        }
-    }
 }
---
+++ IPFEngine/IPFEngine.Parser/Records.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPFEngine.Parser
+{
+    public abstract record IPFVariable(string Name, string Text);
+    public record IPFVariableBoolean(string Name, string Text, bool DefaultValue) : IPFVariable(Name, Text);
+    public record IPFVariableList(string Name, string Text, IList<IPFListItem> Items, string DefaultSymbol) : IPFVariable(Name, Text);
+    public record IPFListItem(string Symbol, string Value);
+    public record IPFVariableNumber(string Name, string Text, int MinValue, int MaxValue, int DefaultValue) : IPFVariable(Name, Text);
+
+    public abstract record IPFItem(IEnumerable<string> Condition);
+    public record IPFFee(string Name, IList<IPFItem> Cases)
+    {
+        public override string ToString()
+        {
+            return string.Format("\n\rFEE: {0}\n\r{1}", Name, string.Join(Environment.NewLine, Cases));
+        }
+    }
+    public record IPFFeeCase(IEnumerable<string> Condition, IList<IPFFeeYield> Yields) : IPFItem(Condition)
+    {
+        public override string ToString()
+        {
+            return string.Format("CASE: {0}\n\r{1}", string.Join(" ", Condition), string.Join(Environment.NewLine, Yields));
+        }
+    }
+    public record IPFFeeYield(IEnumerable<string> Condition, IEnumerable<string> Values) : IPFItem(Condition)
+    {
+        public override string ToString()
+        {
+            return string.Format("YIELD: {0} CONDITION: {1}", string.Join(" ", Values), string.Join(" ", Condition));
+        }
+    }
+}
