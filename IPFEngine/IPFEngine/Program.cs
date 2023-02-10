e4b5f73350b54054dc831ab8792cb590582cfc80
Valer BOCAN <vbocan@gmail.com>
2023-02-10 09:37:39 +0200

    Add colored console output

--- IPFEngine/IPFEngine/Program.cs
+++ IPFEngine/IPFEngine/Program.cs
@@ -1,24 +1,27 @@
 ﻿using IPFEngine.Evaluator;
 using IPFEngine.Parser;
+using Pastel;
+using System.Drawing;
 using System.Linq;
 
 string text = File.ReadAllText(@"..\..\..\us_fees.ipf");
 var p = new IPFParser(text);
 var (Variables, Fees) = p.Parse();
 
-Console.WriteLine("VARIABLES ===============================================");
+Console.WriteLine("PARSED VARIABLES ===============================================".Pastel(ConsoleColor.Yellow));
 foreach (var v in Variables)
 {
     Console.WriteLine(v);
 }
 
-Console.WriteLine("FEES: ===================================================");
+Console.WriteLine();
+Console.WriteLine("PARSED FEES: ===================================================".Pastel(ConsoleColor.Yellow));
 foreach (var f in Fees)
 {
     Console.WriteLine(f);
 }
-
-Console.WriteLine("SEMANTIC CHECK: =========================================");
+Console.WriteLine();
+Console.WriteLine("SEMANTIC CHECK: =========================================".Pastel(ConsoleColor.Yellow));
 var ck = IPFSemanticChecker.Check(Variables, Fees);
 if (!ck.Any())
 {
@@ -32,8 +35,8 @@ else
         Console.WriteLine(e);
     }
 }
-
-Console.WriteLine("FEE COMPUTATION: ========================================");
+Console.WriteLine();
+Console.WriteLine("FEE COMPUTATION: ========================================".Pastel(ConsoleColor.Yellow));
 var vars = new IPFValue[] {
     new IPFValueString("EntityType", "NormalEntity"),
     new IPFValueString("SituationType", "PreparedISA"),
@@ -41,10 +44,17 @@ var vars = new IPFValue[] {
     new IPFValueNumber("ClaimCount", 7)
 };
 
+Console.WriteLine("Input variables:".Pastel(ConsoleColor.White));
+foreach(var v in vars)
+{
+    Console.WriteLine(v.ToString().Pastel(ConsoleColor.Cyan));
+}
+Console.WriteLine();
+
 int TotalAmount = 0;
 foreach (var fee in Fees)
 {
-    Console.WriteLine("COMPUTING FEE [{0}]", fee.Name);
+    Console.WriteLine("COMPUTING FEE [{0}]".Pastel(ConsoleColor.White), fee.Name);
     int Amount = 0;
     Console.WriteLine("Amount is initially {0}", Amount);
     foreach (IPFFeeCase fc in fee.Cases)
@@ -68,9 +78,9 @@ foreach (var fee in Fees)
             }
         }
     }
-    Console.WriteLine("Finally, the amount for fee {0} is {1}", fee.Name, Amount);
+    Console.WriteLine("Finally, the amount for fee {0} is {1}".Pastel(ConsoleColor.White), fee.Name, Amount);
     Console.WriteLine();
     TotalAmount += Amount;
 }
 
-Console.WriteLine("Total amount (for all fees): {0}", TotalAmount);
+Console.WriteLine("Total amount (for all fees): {0}".Pastel(Color.White).PastelBg(Color.Red), TotalAmount);
