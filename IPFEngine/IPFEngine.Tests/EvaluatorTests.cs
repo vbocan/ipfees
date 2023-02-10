b6aa2a72b83eca3dd1dca3014a04ef7b6ac78d15
Valer BOCAN <vbocan@gmail.com>
2023-02-10 04:48:08 +0200

    Add evaluator tests

---
+++ IPFEngine/IPFEngine.Tests/EvaluatorTests.cs
@@ -0,0 +1,67 @@
+using IPFEngine.Evaluator;
+using IPFEngine.Parser;
+using Newtonsoft.Json.Linq;
+
+namespace IPFEngine.Tests
+{
+    public class EvaluatorTests
+    {
+        [Fact]
+        public void TestArithmeticEvaluation()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueNumber("A", 6),
+                new IPFValueNumber("B", 80),
+                new IPFValueNumber("C", 30),
+            };
+            var tokens = "( B + 2 ) * C - 11 * ( B + 2 * A )".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateExpression(tokens, vars);
+            Assert.Equal(1448, ev);
+        }
+
+        [Fact]
+        public void TestInequalityEvaluation()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueNumber("A", 6),                
+            };
+            var tokens = "A EQUALS 60 / 10".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateLogic(tokens, vars);
+            Assert.True(ev);
+        }
+
+        [Fact]
+        public void TestStringEvaluation()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueString("EntityType", "NormalEntity"),
+            };
+            var tokens = "EntityType EQUALS NormalEntity".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateLogic(tokens, vars);
+            Assert.True(ev);
+        }
+
+        [Fact]
+        public void TestLogicEvaluation()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueString("EntityType", "NormalEntity"),
+                new IPFValueNumber("SheetCount", 101),
+            };
+            var tokens = "SheetCount ABOVE 100 AND EntityType EQUALS NormalEntity".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateLogic(tokens, vars);
+            Assert.True(ev);
+        }
+
+        [Fact]
+        public void TestBooleanEvaluation()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueBoolean("ContainsDependentClaims", true),                
+            };
+            var tokens = "ContainsDependentClaims EQUALS TRUE".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateLogic(tokens, vars);
+            Assert.True(ev);
+        }
+    }
+}
