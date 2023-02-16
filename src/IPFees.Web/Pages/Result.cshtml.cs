e778316c04aea5a10f28b9bd0d2435a6d18a8a8b
Valer BOCAN <vbocan@gmail.com>
2023-02-16 23:06:58 +0200

    Computation in page
    Except that there is a glitch in display (shows 0)

--- src/IPFees.Web/Pages/Result.cshtml.cs
+++ src/IPFees.Web/Pages/Result.cshtml.cs
@@ -1,3 +1,6 @@
+using IPFees.Calculator;
+using IPFees.Evaluator;
+using IPFees.Parser;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,20 +8,60 @@ namespace IPFees.Web.Pages
 {
     public class ResultModel : PageModel
     {
+        [BindProperty]
+        public IEnumerable<IPFVariable> Vars { get; set; }
+        [TempData]
+        public int TotalAmount { get; set; }
+        [TempData]
+        public IEnumerable<string> ComputationSteps { get; set; }
+
+        private readonly IIPFCalculator _calc;
+        private readonly ILogger<IndexModel> _logger;
+
+        public ResultModel(IIPFCalculator IPFCalculator, ILogger<IndexModel> logger)
+        {
+            _calc = IPFCalculator;
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+
         }
 
         public IActionResult OnPostDisplay(IFormCollection form)
         {
-            var formData = new Dictionary<string, string>();
+            var Code = (string)TempData["code"];
+            _calc.Parse(Code);
+            Vars = _calc.GetVariables();
 
+            var vars = new List<IPFValue>();
+
+            // Cycle through all form fields
             foreach (var field in form)
             {
-                formData.Add(field.Key, field.Value);
+                var CalcVar = Vars.SingleOrDefault(s => s.Name.Equals(field.Key));
+                if (CalcVar == null) continue;
+                switch (CalcVar)
+                {
+                    case IPFVariableList:
+                        var cv1 = (IPFVariableList)CalcVar;
+                        vars.Add(new IPFValueString(CalcVar.Name, field.Value));
+                        break;
+                    case IPFVariableNumber:
+                        var cv2 = (IPFVariableNumber)CalcVar;
+                        var res2 = int.TryParse(field.Value, out var val2);
+                        vars.Add(new IPFValueNumber(CalcVar.Name, val2));
+                        break;
+                    case IPFVariableBoolean:
+                        var cv3 = (IPFVariableBoolean)CalcVar;
+                        var res3 = bool.TryParse(field.Value, out var val3);
+                        vars.Add(new IPFValueBoolean(CalcVar.Name, val3));
+                        break;
+                }
             }
 
-            // TODO: Process the form data as needed
+            var (TotalAmount, ComputationSteps) = _calc.Compute(vars.ToArray());
 
             return Page();
         }
