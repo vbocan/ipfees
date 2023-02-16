4e75965068d76693169de83479d81f63924b2b6e
Valer BOCAN <vbocan@gmail.com>
2023-02-16 13:13:49 +0200

    Rename ShowResult page to Result

--- src/IPFees.Web/Pages/Result.cshtml.cs
+++ src/IPFees.Web/Pages/Result.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IPFees.Web.Pages
 {
-    public class ShowResultModel : PageModel
+    public class ResultModel : PageModel
     {
         public void OnGet()
         {
