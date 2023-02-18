274699d5539b3e54dc15feb04ecaceb628829120
Valer BOCAN <vbocan@gmail.com>
2023-02-18 21:18:07 +0200

    Variable values shown on result page

--- src/IPFees.Core/Evaluator/Records.cs
+++ src/IPFees.Core/Evaluator/Records.cs
@@ -6,8 +6,26 @@ using System.Threading.Tasks;
 
 namespace IPFees.Evaluator
 {
-    public abstract record IPFValue(string Name);    
-    public record IPFValueString(string Name, string Value) : IPFValue(Name);
-    public record IPFValueNumber(string Name, int Value) : IPFValue(Name);
-    public record IPFValueBoolean(string Name, bool Value) : IPFValue(Name);
+    public abstract record IPFValue(string Name);
+    public record IPFValueString(string Name, string Value) : IPFValue(Name)
+    {
+        public override string ToString()
+        {
+            return string.Format("String: [{0}]", Value);
+        }
+    }
+    public record IPFValueNumber(string Name, int Value) : IPFValue(Name)
+    {
+        public override string ToString()
+        {
+            return string.Format("Number: [{0}]", Value);
+        }
+    };
+    public record IPFValueBoolean(string Name, bool Value) : IPFValue(Name)
+    {
+        public override string ToString()
+        {
+            return string.Format("Boolean: [{0}]", Value);
+        }
+    };
 }
