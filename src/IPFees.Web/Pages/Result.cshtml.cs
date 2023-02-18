57751addaad810e966d626768ca75e18e5d767a0
Valer BOCAN <vbocan@gmail.com>
2023-02-18 21:18:07 +0200

    Variable values shown on result page

--- src/IPFees.Web/Pages/Result.cshtml.cs
+++ src/IPFees.Web/Pages/Result.cshtml.cs
@@ -9,13 +9,13 @@ namespace IPFees.Web.Pages
     public class ResultModel : PageModel
     {
         [BindProperty]
-        public IEnumerable<IPFVariable> Vars { get; set; }
-        [BindProperty]
         public int TotalAmount { get; set; }
         [BindProperty]
         public IEnumerable<string> ComputationSteps { get; set; }
         [BindProperty]
         public string ComputationError { get; set; }
+        [BindProperty]
+        public List<IPFValue> CollectedVars { get; set; }
 
         private readonly IIPFCalculator _calc;
         private readonly ILogger<IndexModel> _logger;
@@ -35,38 +35,39 @@ namespace IPFees.Web.Pages
         {
             string Code = (string)TempData.Peek("code");
             _calc.Parse(Code);
-            Vars = _calc.GetVariables();
+            var ParsedVars = _calc.GetVariables();
 
-            var vars = new List<IPFValue>();
+            CollectedVars = new List<IPFValue>();
 
             // Cycle through all form fields
             foreach (var field in form)
             {
-                var CalcVar = Vars.SingleOrDefault(s => s.Name.Equals(field.Key));
+                var CalcVar = ParsedVars.SingleOrDefault(s => s.Name.Equals(field.Key));
                 if (CalcVar == null) continue;
                 switch (CalcVar)
                 {
                     case IPFVariableList:
                         var cv1 = (IPFVariableList)CalcVar;
-                        vars.Add(new IPFValueString(CalcVar.Name, field.Value));
+                        CollectedVars.Add(new IPFValueString(CalcVar.Name, field.Value));
                         break;
                     case IPFVariableNumber:
                         var cv2 = (IPFVariableNumber)CalcVar;
                         var res2 = int.TryParse(field.Value, out var val2);
-                        vars.Add(new IPFValueNumber(CalcVar.Name, val2));
+                        CollectedVars.Add(new IPFValueNumber(CalcVar.Name, val2));
                         break;
                     case IPFVariableBoolean:
                         var cv3 = (IPFVariableBoolean)CalcVar;
                         var res3 = bool.TryParse(field.Value, out var val3);
-                        vars.Add(new IPFValueBoolean(CalcVar.Name, val3));
+                        CollectedVars.Add(new IPFValueBoolean(CalcVar.Name, val3));
                         break;
                 }
             }
 
             try
             {
-                (TotalAmount, ComputationSteps) = _calc.Compute(vars.ToArray());
-            }catch(Exception ex)
+                (TotalAmount, ComputationSteps) = _calc.Compute(CollectedVars.ToArray());
+            }
+            catch (Exception ex)
             {
                 ComputationError = ex.Message;
             }
