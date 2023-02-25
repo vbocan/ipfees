e0f71524eae756c06e630af296b28f01244a3842
Valer BOCAN <vbocan@gmail.com>
2023-02-25 19:25:10 +0200

    Add tests for functions FLOOR, ROUND, CEIL

--- src/IPFees.Tests/ExpressionEvaluatorTests.cs
+++ src/IPFees.Tests/ExpressionEvaluatorTests.cs
@@ -29,5 +29,43 @@ namespace IPFees.Tests
             var ev = IPFEvaluator.EvaluateExpression(tokens, vars);
             Assert.Equal(-10, ev);
         }
+
+        [Fact]
+        public void TestFunctionFloor()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueNumber("A", 40.7),
+            };
+            var tokens = "FLOOR ( A )".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateExpression(tokens, vars);
+            Assert.Equal(40, ev);
+        }
+
+        [Fact]
+        public void TestFunctionRound()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueNumber("A", 40.3),
+                new IPFValueNumber("B", 40.7),
+            };
+            var tokens1 = "ROUND ( A )".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev1 = IPFEvaluator.EvaluateExpression(tokens1, vars);
+            Assert.Equal(40, ev1);
+
+            var tokens2 = "ROUND ( B )".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev2 = IPFEvaluator.EvaluateExpression(tokens2, vars);
+            Assert.Equal(41, ev2);
+        }
+
+        [Fact]
+        public void TestFunctionCeil()
+        {
+            var vars = new IPFValue[] {
+                new IPFValueNumber("A", 40.3),
+            };
+            var tokens = "CEIL ( A )".Split(new char[] { ' ' }, StringSplitOptions.None);
+            var ev = IPFEvaluator.EvaluateExpression(tokens, vars);
+            Assert.Equal(41, ev);
+        }
     }
 }
