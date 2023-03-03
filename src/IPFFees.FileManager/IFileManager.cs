e6f327199318e046a80e4c6e7a685432a3a8f24b
Valer BOCAN <vbocan@gmail.com>
2023-03-03 15:26:01 +0200

    File manager skeleton

---
+++ src/IPFFees.FileManager/IFileManager.cs
@@ -0,0 +1,10 @@
+﻿namespace IPFFees.FileManager
+{
+    public interface IFileManager
+    {
+        public bool AddFile(string fileName, string content);
+        public bool RemoveFile(string fileName);
+        public IEnumerable<string> GetFiles();
+        public string GetFileContents(string fileName);
+    }
+}
