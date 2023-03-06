1c30723357487ffcdecd925e2e86e9e687cd2df2
Valer BOCAN <vbocan@gmail.com>
2023-03-06 19:19:11 +0200

    Tests for module

---
+++ src/IPFees.Core.Tests/Fixture/CoreFixture.cs
@@ -0,0 +1,29 @@
+﻿using IPFees.Core.Helpers;
+using IPFFees.Core.Data;
+using IPFFees.Core.Models;
+using MongoDB.Driver;
+
+namespace IPFees.Core.Tests.Fixture
+{
+    public class CoreFixture : IDisposable
+    {
+        public DataContext DbContext { get; private set; }
+        public IDateTimeHelper DateHelper { get; private set; }
+        public IMongoCollection<ModuleDoc> ModuleCollection { get; private set; }
+        private readonly string connectionString = "mongodb+srv://abdroot:Test123@cluster0.dusbo.mongodb.net/IPFees?retryWrites=true&w=majority";
+
+        public CoreFixture()
+        {
+            // Build database context based on the connection string
+            DbContext = new DataContext(connectionString);
+            ModuleCollection = DbContext.ModuleCollection;
+            DateHelper = new DateTimeHelper();
+            DbContext.DropDatabase();
+        }
+
+        public void Dispose()
+        {
+            DbContext.DropDatabase();
+        }
+    }
+}
---
+++ src/IPFees.Core/Helpers/DateTimeHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPFees.Core.Helpers
+{
+    public class DateTimeHelper : IDateTimeHelper
+    {
+        public DateTime GetDateTimeNow() =>  DateTime.UtcNow.ToLocalTime();
+    }
+}
