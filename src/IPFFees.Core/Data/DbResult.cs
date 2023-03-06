66ab42581aa05e7fc4e14536e0519c56c99fd801
Valer BOCAN <vbocan@gmail.com>
2023-03-06 10:40:31 +0200

    Module management

---
+++ src/IPFFees.Core/Data/DbResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPFFees.Core.Data
+{
+    public class DbResult
+    {
+        public bool Success { get; set; }
+        public string? Reason { get; set; }
+        public Guid Id { get; set; }
+        public static DbResult Succeed() => new() { Success = true };
+        public static DbResult Succeed(Guid Id) => new() { Success = true, Id = Id };
+        public static DbResult Fail() => new() { Success = false };
+        public static DbResult Fail(string Reason) => new() { Success = false, Reason = Reason };
+        public static DbResult Fail(Exception ex) => new() { Success = false, Reason = ex.Message };
+
+    }
+}
---
+++ src/IPFFees.Core/ModuleInfo.cs
@@ -0,0 +1,4 @@
+﻿namespace IPFFees.Core
+{
+    public record ModuleInfo(string Name, string Description, string Contents, DateTime LastUpdatedOn);
+}
