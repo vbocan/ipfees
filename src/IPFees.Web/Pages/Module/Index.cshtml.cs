88527c9d483bec3b1fce76cf7992c305ca991d5f
Valer BOCAN <vbocan@gmail.com>
2023-03-08 19:20:16 +0200

    Add module index page

---
+++ src/IPFees.Web/Pages/Module/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IPFees.Web.Pages.Module
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
