685bac8df71e9c9eb7069a973dfeb5882ecef4e3
Valer BOCAN <vbocan@gmail.com>
2023-03-08 11:26:52 +0200

    Add jurisdictions

---
+++ src/IPFees.Core/IJurisdiction.cs
@@ -0,0 +1,14 @@
+﻿using IPFFees.Core.Data;
+
+namespace IPFFees.Core
+{
+    public interface IJurisdiction
+    {
+        Task<DbResult> AddJurisdictionAsync(string JurisdictionName);
+        Task<DbResult> SetJurisdictionSourceCodeAsync(string JurisdictionName, string SourceCode);
+        Task<DbResult> RemoveJurisdictionAsync(string JurisdictionName);
+        IEnumerable<JurisdictionInfo> GetJurisdictions();
+        JurisdictionInfo GetJurisdictionByName(string JurisdictionName);
+        Task<DbResult> SetReferencedModules(string JurisdictionName, string[] ModuleNames);
+    }
+}
