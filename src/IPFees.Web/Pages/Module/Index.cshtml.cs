7a765fdd052f1cec7bd6c22ce4031aee4a90a8c1
Valer BOCAN <vbocan@gmail.com>
2023-03-09 16:54:21 +0200

    Provision index for the module page

--- src/IPFees.Web/Pages/Module/Index.cshtml.cs
+++ src/IPFees.Web/Pages/Module/Index.cshtml.cs
@@ -1,12 +1,24 @@
+using IPFees.Evaluator;
+using IPFees.Web.Data;
+using IPFFees.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace IPFees.Web.Pages.Module
 {
     public class IndexModel : PageModel
     {
+        [BindProperty] public IEnumerable<ModuleInfo> Modules { get; set; }
+        private readonly IModuleRepository moduleRepository;
+
+        public IndexModel(IModuleRepository moduleRepository)
+        {
+            this.moduleRepository = moduleRepository;
+        }
         public void OnGet()
         {
+            Modules = moduleRepository.GetModules();
         }
     }
 }
