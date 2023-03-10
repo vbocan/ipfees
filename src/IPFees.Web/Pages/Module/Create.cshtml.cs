bd3d0caae6aa5da4b41aa6c2151437bc747db26e
Valer BOCAN <vbocan@gmail.com>
2023-03-10 10:02:25 +0200

    Complete module CRUD functionality

--- src/IPFees.Web/Pages/Module/Create.cshtml.cs
+++ src/IPFees.Web/Pages/Module/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using IPFFees.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,43 @@ namespace IPFees.Web.Pages.Module
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        [BindProperty] public string Name { get; set; }
+        [BindProperty] public string Description { get; set; }
+        [BindProperty] public string SourceCode { get; set; }
+        [BindProperty] public IList<string> ErrorMessages { get; set; }
+
+        private readonly IModuleRepository moduleRepository;
+
+        public CreateModel(IModuleRepository moduleRepository)
         {
+            this.moduleRepository = moduleRepository;
+            ErrorMessages = new List<string>();
+        }
+
+        public void OnGet()
+        {            
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var res = await moduleRepository.AddModuleAsync(Name);
+            if (!res.Success)
+            {
+                ErrorMessages.Add($"Error creating module: {res.Reason}");
+            }
+            res = await moduleRepository.SetModuleDescriptionAsync(Name, Description);
+            if (!res.Success)
+            {
+                ErrorMessages.Add($"Error setting module description: {res.Reason}");
+            }
+            res = await moduleRepository.SetModuleSourceCodeAsync(Name, SourceCode);
+            if (!res.Success)
+            {
+                ErrorMessages.Add($"Error setting module source code: {res.Reason}");
+            }
+
+            if (ErrorMessages.Any()) return Page();
+            else return RedirectToPage("Index");
         }
     }
 }
