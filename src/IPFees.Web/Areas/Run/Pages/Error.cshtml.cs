a72cecdcaf81dfcbbc2919fbdebe281090fded72
Valer BOCAN <vbocan@gmail.com>
2023-03-11 08:27:14 +0200

    Move sections to own areas

---
+++ src/IPFees.Web/Areas/Run/Pages/Error.cshtml.cs
@@ -0,0 +1,23 @@
+using IPFees.Evaluator;
+using IPFees.Web.Data;
+using IPFFees.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace IPFees.Web.Areas.Run.Pages
+{
+    public class ErrorModel : PageModel
+    {
+        [BindProperty] public IEnumerable<string> Errors { get; set; }
+
+        public ErrorModel()
+        {
+        }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Errors = (IEnumerable<string>)TempData["ParseErrors"];
+            return Page();
+        }
+    }
+}
---
+++ src/IPFees.Web/Areas/Run/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+using IPFees.Calculator;
+using IPFees.Evaluator;
+using IPFees.Parser;
+using IPFFees.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static IPFees.Core.OfficialFee;
+
+namespace IPFees.Web.Areas.Run.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public IEnumerable<DslVariable> Vars { get; set; }
+
+        private readonly IOfficialFee officialFee;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(IOfficialFee officialFee, ILogger<IndexModel> logger)
+        {
+            this.officialFee = officialFee;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {            
+            var res = await officialFee.GetVariables(id);
+            if (!res.IsSuccessfull)
+            {
+                TempData["ParseErrors"] = (res as OfficialFeeResultFail).Errors.ToList();
+                return RedirectToPage("Error");
+            }            
+            Vars = (res as OfficialFeeParseSuccess).RequestedVariables;
+            return Page();
+        }
+    }
+}
