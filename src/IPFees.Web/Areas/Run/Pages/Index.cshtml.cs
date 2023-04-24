803d8e131edde11358be0a81794a3a5828f5d2da
Valer BOCAN <vbocan@gmail.com>
2023-04-24 15:53:25 +0300

    Support for data collection (WIP)

--- src/IPFees.Web/Areas/Run/Pages/Index.cshtml.cs
+++ src/IPFees.Web/Areas/Run/Pages/Index.cshtml.cs
@@ -5,43 +5,36 @@ using IPFFees.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics.Eventing.Reader;
+using System.Linq;
 using static IPFees.Core.OfficialFee;
 
 namespace IPFees.Web.Areas.Run.Pages
 {
     public class IndexModel : PageModel
-    {        
+    {
         [BindProperty] public IEnumerable<JurisdictionInfo> Jurisdictions { get; set; }
-        [BindProperty] public IList<JurisdictionViewModel> SelectedJurisdictions { get; set; }
-        //indProperty] public IEnumerable<string> Errors { get; set; }
+        [BindProperty] public IList<JurisdictionViewModel> SelectedJurisdictions { get; set; }        
 
-        private readonly IJurisdictionRepository jurisdictionRepository;
-        private readonly IOfficialFee officialFee;
+        private readonly IJurisdictionRepository jurisdictionRepository;        
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(IJurisdictionRepository jurisdictionRepository, IOfficialFee officialFee, ILogger<IndexModel> logger)
+        public IndexModel(IJurisdictionRepository jurisdictionRepository, ILogger<IndexModel> logger)
         {
-            this.jurisdictionRepository = jurisdictionRepository;
-            this.officialFee = officialFee;
+            this.jurisdictionRepository = jurisdictionRepository;            
             _logger = logger;
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            var DbJur = await jurisdictionRepository.GetJurisdictions();
-            Jurisdictions = DbJur.OrderBy(o => o.Name);
-            SelectedJurisdictions = DbJur.OrderBy(o=>o.Name).Select(s => new JurisdictionViewModel(s.Id, s.Name, s.Description, true)).ToList();
+            Jurisdictions = await jurisdictionRepository.GetJurisdictions();
+            SelectedJurisdictions = Jurisdictions.OrderBy(o => o.Name).Select(s => new JurisdictionViewModel(s.Id, s.Name, s.Description, true)).ToList();
             return Page();
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            // Get selected jurisdictions
-            var SelJur = SelectedJurisdictions.Where(w => w.Checked).Select(s => s.Id.ToString()).ToList();
-            // TODO: For each selected jurisdiction, get the needed inputs
-            // TODO: Remove duplicate inputs, such that one given input is only displayed once
-            // TODO: Perform calculation and display results
-            return Page1();
+            var SelJur = await SelectedJurisdictions.ToAsyncEnumerable().Where(w => w.Checked).Select(s => s.Id).ToListAsync();
+            return RedirectToPage("DataCollect", new { area = "Run", Id = SelJur });
         }
     }
 
