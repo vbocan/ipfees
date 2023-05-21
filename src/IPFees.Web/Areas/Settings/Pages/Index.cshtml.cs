d4190170459bcb3daa64295c7e6823013a941fc6
Valer BOCAN <vbocan@gmail.com>
2023-05-21 23:32:36 +0300

    Update Index.cshtml.cs

--- src/IPFees.Web/Areas/Settings/Pages/Index.cshtml.cs
+++ src/IPFees.Web/Areas/Settings/Pages/Index.cshtml.cs
@@ -13,28 +13,27 @@ namespace IPFees.Web.Areas.Settings.Pages
         [BindProperty] public IEnumerable<AttorneyFeeInfo> AttorneyFees { get; set; }
 
         private readonly IKeyValueRepository keyvalueRepository;
-        private readonly IModuleRepository moduleRepository;        
+        private readonly IModuleRepository moduleRepository;
 
         public IndexModel(IKeyValueRepository keyvalueRepository, IModuleRepository moduleRepository)
         {
-            this.keyvalueRepository = keyvalueRepository;            
+            this.keyvalueRepository = keyvalueRepository;
             this.moduleRepository = moduleRepository;
         }
         public async Task<IActionResult> OnGetAsync()
         {
             // Retrieve the distinct list of categories
-            ModuleCategories = from s in (await moduleRepository.GetModules()).DistinctBy(d => d.Category)
-                               let cd = keyvalueRepository.GetCategoryAsync(s.Category).Result
-                               orderby cd.Item1 ascending
-                               select new ModuleCategoryInfo(s.Name, cd.Item1, cd.Item2);
-                
+            var mca = from s in (await moduleRepository.GetModules()).DistinctBy(d => d.Category)
+                     let cd = keyvalueRepository.GetCategoryAsync(s.Category).Result
+                     orderby cd.Item1 ascending
+                     select new ModuleCategoryInfo(s.Category, cd.Item1, cd.Item2);
+            ModuleCategories = mca.ToList();
             // Retrieve the list of attorney fee levels
-            AttorneyFees = from s in Enum.GetValues(typeof(JurisdictionAttorneyFeeLevel)).Cast<JurisdictionAttorneyFeeLevel>()
-                           let af = keyvalueRepository.GetAttorneyFeeAsync(s).Result
-                           select new AttorneyFeeInfo(s, af.Item1, af.Item2);
+            var afs = from s in Enum.GetValues(typeof(JurisdictionAttorneyFeeLevel)).Cast<JurisdictionAttorneyFeeLevel>()
+                     let af = keyvalueRepository.GetAttorneyFeeAsync(s).Result
+                     select new AttorneyFeeInfo(s, af.Item1, af.Item2);
+            AttorneyFees = afs.ToList();
             return Page();
         }
     }
-
-    public record ModuleViewModel(ModuleInfo Mod, int Count);
 }
