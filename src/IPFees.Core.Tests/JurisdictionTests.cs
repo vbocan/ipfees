9e1c98e0855a69290eac0cb51687017fd3c4a07b
Valer BOCAN <vbocan@gmail.com>
2023-05-25 18:30:46 +0300

    Add jurisdiction repository and tests

---
+++ src/IPFees.Core.Tests/JurisdictionTests.cs
@@ -0,0 +1,84 @@
+using IPFees.Core.Tests.Fixture;
+using IPFees.Core;
+using IPFees.Core.Data;
+using IPFees.Core.Repository;
+using IPFees.Core.Enum;
+
+namespace IPFees.Core.Tests
+{
+    public class JurisdictionTests : IClassFixture<CoreFixture>
+    {
+        private readonly CoreFixture fixture;
+
+        public JurisdictionTests(CoreFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async void AddJurisdictionTest()
+        {
+            var jur = new JurisdictionRepository(fixture.DbContext);
+            var res1 = await jur.AddJurisdictionAsync("Jurisdiction-Add");
+            Assert.True(res1.Success);
+            var res2 = await jur.GetJurisdictionById(res1.Id);
+            Assert.True(res2 != null);
+        }
+
+        [Fact]
+        public async void AddDuplicateJurisdictionTest()
+        {
+            var jur = new JurisdictionRepository(fixture.DbContext);
+            var res1 = await jur.AddJurisdictionAsync("Jurisdiction-DuP");
+            var res2 = await jur.AddJurisdictionAsync("Jurisdiction-DuP");
+            Assert.True(res1.Success);
+            Assert.False(res2.Success);
+        }
+
+        [Fact]
+        public async void SetJurisdictionNameTest()
+        {
+            var jur = new JurisdictionRepository(fixture.DbContext);
+            var res1 = await jur.AddJurisdictionAsync("Jurisdiction-Set-Name");
+            Assert.True(res1.Success);
+            var res2 = await jur.SetJurisdictionNameAsync(res1.Id, "Jurisdiction Name");
+            Assert.True(res2.Success);
+            var mi = await jur.GetJurisdictionById(res1.Id);
+            Assert.Equal("Jurisdiction Name", mi.Name);
+        }
+
+        [Fact]
+        public async void SetJurisdictionDescriptionTest()
+        {
+            var jur = new JurisdictionRepository(fixture.DbContext);
+            var res1 = await jur.AddJurisdictionAsync("Jurisdiction-Set-Description");
+            Assert.True(res1.Success);
+            var res2 = await jur.SetJurisdictionDescriptionAsync(res1.Id, "Jurisdiction Description");
+            Assert.True(res2.Success);
+            var mi = await jur.GetJurisdictionById(res1.Id);
+            Assert.Equal("Jurisdiction Description", mi.Description);
+        }
+        
+        [Fact]
+        public async void SetJurisdictionAttorneyFeeLevelTest()
+        {
+            var jur = new JurisdictionRepository(fixture.DbContext);
+            var res1 = await jur.AddJurisdictionAsync("Jurisdiction-Set-FeeLevel");
+            Assert.True(res1.Success);
+            var res2 = await jur.SetJurisdictionAttorneyFeeLevelAsync(res1.Id, AttorneyFeeLevel.Level3);
+            Assert.True(res2.Success);
+            var mi = await jur.GetJurisdictionById(res1.Id);
+            Assert.Equal(AttorneyFeeLevel.Level3, mi.AttorneyFeeLevel);
+        }
+
+        [Fact]
+        public async void RemoveJurisdictionsTest()
+        {
+            var jur = new JurisdictionRepository(fixture.DbContext);
+            var res1 = await jur.AddJurisdictionAsync("Jurisdiction-Del");
+            var res2 = await jur.RemoveJurisdictionAsync(res1.Id);
+            Assert.True(res1.Success);
+            Assert.True(res2.Success);
+        }
+    }
+}
---
+++ src/IPFees.Core/Model/JurisdictionDoc.cs
@@ -0,0 +1,27 @@
+﻿using IPFees.Core.Enum;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace IPFees.Core.Model
+{
+    [BsonIgnoreExtraElements]
+    public class JurisdictionDoc
+    {
+        public const string CollectionName = "Jurisdictions";
+
+        public JurisdictionDoc()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+            AttorneyFeeLevel = AttorneyFeeLevel.Level1;
+            LastUpdatedOn = DateTime.MinValue;
+        }
+
+        [BsonId]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }        
+        public AttorneyFeeLevel AttorneyFeeLevel { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime LastUpdatedOn { get; set; }
+    }
+}
