24ea2a8c93748c5352c48c7494c64679f28f0afd
Valer BOCAN <vbocan@gmail.com>
2023-08-29 20:01:04 +0300

    Filter available currencies (for user)

---
+++ src/IPFees.Web/Data/Currency.cs
@@ -0,0 +1,9 @@
+﻿namespace IPFees.Web.Data
+{
+    public class CurrencySettings
+    {
+        public const string SectionName = nameof(CurrencySettings);
+        public string[] AllowedCurrencies { get; set; }
+        public double CurrencyMarkup { get; set; }
+    }
+}
