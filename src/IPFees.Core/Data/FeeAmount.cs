d65f87142b80777832e4050baff07672e0c29b4e
Valer BOCAN <vbocan@gmail.com>
2023-08-30 06:34:30 +0300

    Move class FeeAmount to own file

---
+++ src/IPFees.Core/Data/FeeAmount.cs
@@ -0,0 +1,36 @@
+﻿namespace IPFees.Core.Data
+{
+    public class FeeAmount
+    {
+        public double MandatoryAmount { get; set; }
+        public double OptionalAmount { get; set; }
+        public string Currency { get; set; }
+
+        public FeeAmount(double mandatoryAmount, double optionalAmount, string currency)
+        {
+            MandatoryAmount = mandatoryAmount;
+            OptionalAmount = optionalAmount;
+            Currency = currency;
+        }
+
+        public static FeeAmount Add(FeeAmount first, FeeAmount second)
+        {
+            if (first.Currency != second.Currency)
+            {
+                // TODO: Uncomment this in production
+                //throw new ArgumentException("Currencies are not the same.");
+            }
+
+            double totalMandatory = first.MandatoryAmount + second.MandatoryAmount;
+            double totalOptional = first.OptionalAmount + second.OptionalAmount;
+
+            return new FeeAmount(totalMandatory, totalOptional, first.Currency);
+        }
+
+        public override string ToString()
+        {
+            return $"Mandatory Amount: {MandatoryAmount} {Currency}\n" +
+                   $"Optional Amount: {OptionalAmount} {Currency}";
+        }
+    }
+}
