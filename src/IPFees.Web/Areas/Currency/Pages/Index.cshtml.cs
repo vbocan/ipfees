9bdad83aa0b965850f0a13845645885c3ab98519
Valer BOCAN <vbocan@gmail.com>
2023-09-17 15:07:25 +0300

    Display exchange rate on the Currency page

--- src/IPFees.Web/Areas/Currency/Pages/Index.cshtml.cs
+++ src/IPFees.Web/Areas/Currency/Pages/Index.cshtml.cs
@@ -6,13 +6,14 @@ using Microsoft.Extensions.Options;
 using IPFees.Core.Repository;
 using IPFees.Core.Model;
 using IPFees.Core.Enum;
+using System.Linq;
 
 namespace IPFees.Web.Areas.Currency.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly SharedExchangeRateData serd;
-        public IEnumerable<(string, string)> Currencies { get; set; }
+        public IEnumerable<(string, string, decimal?)> Currencies { get; set; }
         public DateTime ExchangeDataLastUpdatedOn { get; set; }
 
         public IndexModel(SharedExchangeRateData serd)
@@ -21,10 +22,25 @@ namespace IPFees.Web.Areas.Currency.Pages
         }
         public async Task<IActionResult> OnGetAsync()
         {
-            Currencies = serd.GetCurrencies();            
             ExchangeDataLastUpdatedOn = serd.Response.LastUpdatedOn;
-            var amount = serd.ConvertCurrency(100, "USD", "RON");
+            Currencies = GetCurrenciesWithExchangeRate();
             return Page();
         }
-    }    
+
+        private IEnumerable<(string, string, decimal?)> GetCurrenciesWithExchangeRate()
+        {
+            foreach (var curr in serd.GetCurrencies())
+            {
+                var Currency = curr.Item1;
+                var Description = curr.Item2;
+                decimal? ExchangeRate = null;
+                try
+                {
+                    ExchangeRate = Math.Round(serd.ConvertCurrency(1, Currency, "EUR"), 4);
+                }
+                catch (Exception) { }
+                yield return (Currency, Description, ExchangeRate);
+            }
+        }
+    }
 }
