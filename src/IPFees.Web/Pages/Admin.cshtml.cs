e067a45e1d1367d30568e3fbe472406cfcb2894b
Valer BOCAN <vbocan@gmail.com>
2023-10-11 19:18:56 +0300

    Reorganize admin and index pages

---
+++ src/IPFees.Web/Pages/Admin.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using IPFees.Core.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IPFees.Web.Pages
+{
+    public class AdminModel : PageModel
+    {
+        public int JurisdictionCount { get; set; }
+        public int FeeCount { get; set; }
+        public int ModuleCount { get; set; }
+
+        private readonly IJurisdictionRepository jurisdictionRepository;
+        private readonly IModuleRepository moduleRepository;
+        private readonly IFeeRepository feeRepository;        
+        private readonly ILogger<IndexModel> _logger;
+
+        public AdminModel(IJurisdictionRepository jurisdictionRepository, IModuleRepository moduleRepository, IFeeRepository feeRepository, ILogger<IndexModel> logger)
+        {            
+            this.jurisdictionRepository = jurisdictionRepository;
+            this.moduleRepository = moduleRepository;
+            this.feeRepository = feeRepository;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            JurisdictionCount = (await jurisdictionRepository.GetJurisdictions()).Count();
+            FeeCount = (await feeRepository.GetFees()).Count();
+            ModuleCount = (await moduleRepository.GetModules()).Count();            
+            return Page();
+        }
+    }
+}
--- src/IPFees.Web/Pages/Index.cshtml.cs
+++ src/IPFees.Web/Pages/Index.cshtml.cs
@@ -10,29 +10,13 @@ namespace IPFees.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public int JurisdictionCount { get; set; }
-        public int FeeCount { get; set; }
-        public int ModuleCount { get; set; }
-
-        private readonly IJurisdictionRepository jurisdictionRepository;
-        private readonly IModuleRepository moduleRepository;
-        private readonly IFeeRepository feeRepository;        
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(IJurisdictionRepository jurisdictionRepository, IModuleRepository moduleRepository, IFeeRepository feeRepository, ILogger<IndexModel> logger)
-        {            
-            this.jurisdictionRepository = jurisdictionRepository;
-            this.moduleRepository = moduleRepository;
-            this.feeRepository = feeRepository;
-            _logger = logger;
+        public IndexModel()
+        {
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            JurisdictionCount = (await jurisdictionRepository.GetJurisdictions()).Count();
-            FeeCount = (await feeRepository.GetFees()).Count();
-            ModuleCount = (await moduleRepository.GetModules()).Count();            
-            return Page();
+            return RedirectToPage("Index", new { area = "Run" });
         }
     }
 }
