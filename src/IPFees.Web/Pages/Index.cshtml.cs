02ec69fcd0175837122f1221474ca4e867119114
Valer BOCAN <vbocan@gmail.com>
2023-10-11 19:19:09 +0300

    Update Index.cshtml.cs

--- src/IPFees.Web/Pages/Index.cshtml.cs
+++ src/IPFees.Web/Pages/Index.cshtml.cs
@@ -1,10 +1,5 @@
-﻿using IPFees.Calculator;
-using IPFees.Web.Areas.Fee.Pages;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using IPFees.Core.Repository;
-using IPFees.Core.CurrencyConversion;
-using IPFees.Web.Data;
 
 namespace IPFees.Web.Pages
 {
