41c8681c202497db1d2988d3e7f845737fac7757
Valer BOCAN <vbocan@gmail.com>
2025-08-11 09:04:00 +0300

    Add IPFLang formal grammar page and image
    Introduces a new Grammar page displaying the IPFLang formal grammar in BNF, adds its supporting page model, and includes a related image. Also updates Admin.cshtml to link to the new grammar page and corrects a typo.

---
+++ src/IPFees.Web/Pages/Grammar.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IPFees.Web.Pages
+{
+    public class GrammarModel : PageModel
+    {
+        private readonly ILogger<GrammarModel> _logger;
+
+        public GrammarModel(ILogger<GrammarModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
