f2bfb2dc39e05fa97a3ce1c081bb6c5f5a5bc448
Valer BOCAN <vbocan@gmail.com>
2025-08-11 09:15:39 +0300

    Add new pages to sitemap items list
    Added /Jurisdiction, /Fee, /Settings, /Currency, /Reference, and /Grammar to the sitemap items for improved site coverage.

--- src/IPFees.Web/Pages/Sitemap.cshtml.cs
+++ src/IPFees.Web/Pages/Sitemap.cshtml.cs
@@ -8,7 +8,14 @@ namespace IPFees.Web.Pages
     {
         private readonly List<SitemapItem> _sitemapItems = new List<SitemapItem>
         {
-            new SitemapItem("/Run", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),            
+            new SitemapItem("/Run", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+            new SitemapItem("/Jurisdiction", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+            new SitemapItem("/Fee", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+            new SitemapItem("/Settings", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+            new SitemapItem("/Currency", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+            new SitemapItem("/Reference", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+            new SitemapItem("/Grammar", changefreq: SitemapChangeFrequency.Monthly, priority: 1.0),
+
             // TODO: Add more sitemap items
         };
         private readonly IHttpContextAccessor httpContextAccessor;
